7eddef1e803fed676c6df9ea68eaffa45c432e6c
Tyler Newman <tylerjnewman18@gmail.com>
2020-06-15 18:30:30 -0700

    Add Contact Details

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,11 +2,21 @@ import React from "react";
 
 const ListContacts = ({ contacts }) => {
   return (
-    <ul>
+    <ol>
       {contacts.map((contact) => (
-        <li key={contact.id}>{contact.name}</li>
+        <li key={contact.id} className="contact-list-item">
+          <div
+            className="contact-avatar"
+            style={{ backgroundImage: `url(${contact.avatarURL})` }}
+          ></div>
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.handle}</p>
+          </div>
+          <div className="contact-remove">Remove</div>
+        </li>
       ))}
-    </ul>
+    </ol>
   );
 };
 
