9cc4b5058691b9109b9f2c37153a87e6f598f832
Tyler Newman <tylerjnewman18@gmail.com>
2020-06-17 17:25:46 -0700

    Add react-hook-form

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ImageInput from "./ImageInput";
+import { useForm } from "react-hook-form";
+
+function onSubmitForm(formData) {
+  alert(formData.name + formData.handle);
+}
 
 const CreateContact = () => {
+  const { register, handleSubmit } = useForm();
   return (
     <div>
       <Link className="close-create-contact" to="/">
         Close
       </Link>
-      <form className="create-contact-form">
+      <form
+        className="create-contact-form"
+        onSubmit={handleSubmit(onSubmitForm)}
+      >
         <ImageInput
           className="create-contact-avatar-input"
           name="avatarURL"
           maxHeight={64}
         />
         <div className="create-contact-details">
-          <input type="text" name="name" placeholder="Name" />
-          <input type="text" name="handle" placeholder="Handle" />
-          <button>Add Contact</button>
+          <input type="text" name="name" ref={register} placeholder="Name" />
+          <input
+            type="text"
+            name="handle"
+            ref={register}
+            placeholder="Handle"
+          />
+          <input type="submit" value="Add Contact" />
         </div>
       </form>
     </div>
